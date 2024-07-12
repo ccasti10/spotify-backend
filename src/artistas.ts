357f906f64b96a6194012cacec8cc560f723fbdc
Christian Castillo <cnicolascastillop@gmail.com>
2024-07-11 21:09:51 -0400

    despliega los artistas

---
+++ src/artistas.ts
@@ -0,0 +1,18 @@
+export class Artista {
+  constructor(
+    public id: number,
+    public nombre: string,
+    public bio: string,
+    public tipo: string,
+    public paisOrigen: string,
+    public generoMusical: string[],
+    public oyentesMensuales: number,
+  ) {}
+}
+
+export class Cancion {
+    constructor(
+        public 
+
+    )
+}
